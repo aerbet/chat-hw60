ccbba35b4f61841e000ff52cfa026d169ef0d796
Erbol Zhaparov <ae270718@gmail.com>
2023-11-20 18:13:53 +0600

    completed app, added basic styles for chat and inputs, fixed issues with any type and useEffect, added props

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,92 @@
-import { useState } from 'react'
+import React, {useEffect, useState } from 'react'
 import ChatBox from "./components/ChatBox/ChatBox.tsx";
 import Form from "./components/Form/Form.tsx";
 import './App.css'
 
+interface FormData {
+  message: string;
+  author: string;
+}
+
+interface Message {
+  message: string;
+  author: string;
+  datetime: string;
+}
+
 const App = () => {
-  const [messages, setMessages] = useState([])
+  const [messages, setMessages] = useState<Message[]>([])
   const [datetime, setDatetime] = useState(null)
+
+  const endpointURL = 'http://146.185.154.90:8000/messages';
+  const intervalRef = React.useRef(null)
+
+  const getNewMessages = (datetimeParam: string | null) => {
+    let url = endpointURL;
+    if (datetimeParam !== null) {
+      url = endpointURL + '?datetime=' + datetimeParam;
+    }
+
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Requestt failed');
+      })
+      .then(result => {
+        if (result.length !== 0) {
+          let newData = [...result];
+          newData = newData.reverse();
+          const updatedMessages = newData.concat(messages);
+          const updatedDatetime = result[result.length - 1].datetime;
+          setMessages(updatedMessages);
+          setDatetime(updatedDatetime);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching messages:', error);
+      });
+  };
+
+  useEffect(() => {
+      intervalRef.current
+      setInterval(() => {
+        getNewMessages(datetime);
+      }, 3000);
+
+      return () => {
+        if (intervalRef.current !== null) {
+          window.clearInterval(intervalRef.current);
+        }
+      };
+    },
+    [datetime]);
+
+  const publishMessage = (formData: FormData) => {
+    const data = new URLSearchParams();
+    data.set('message', formData.message);
+    data.set('author', formData.author);
+
+    fetch(endpointURL, {
+      method: 'POST',
+      body: data,
+    })
+      .then(response => {
+        if(!response.ok) {
+          throw new Error('Failed to publish message');
+        }
+      })
+      .catch(error => {
+        console.error('Error publishing message', error);
+      });
+  };
 
 
   return (
     <>
       <div className="App">
-        <Form />
+        <Form publishMessage={publishMessage}/>
         <ChatBox messages={messages}/>
       </div>
     </>
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './Form.css'
 
-const Form: React.FC = () => {
+interface FormProps {
+  publishMessage: (formData: { message: string; author: string }) => void;
+}
+
+const Form: React.FC<FormProps> = ({ publishMessage }) => {
+  const [message, setMessage] = useState<string | null>(null)
+  const [author, setAuthor] = useState<string | null>(null)
+
+  const updateMessage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+  }
+
+  const updateAuthor = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAuthor(event.target.value);
+  }
+
+  const submitForm = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (message && author) {
+      publishMessage({ message, author });
+    }
+  }
   return (
-    <div>
+    <>
+      <form onSubmit={submitForm}>
+        <label htmlFor="message">Message:</label>
+        <input type="text" id="message" placeholder="Some message ..." onChange={updateMessage} />
 
-    </div>
+        <label htmlFor="author">Author:</label>
+        <input type="text" id="author" placeholder="Student" onChange={updateAuthor} />
+        <button type="submit" id="btn-addMsg" className="btn btn-info">Send</button>
+      </form>
+    </>
   );
 };
 
